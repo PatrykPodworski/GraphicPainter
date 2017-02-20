85d04efd86f04eecd816ecb76a49520f1d569baa
PatrykPodworski <patrykpodworski@gmail.com>
2017-02-20 03:03:19 +0100

    RgbImage class

---
+++ GenericPainter/Other/RGBImage.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericPainter.Other
+{
+    public class RgbImage
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public byte[] Red { get; set; }
+        public byte[] Green { get; set; }
+        public byte[] Blue { get; set; }
+
+        public RgbImage(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            Red = new byte[width * height];
+            Green = new byte[width * height];
+            Blue = new byte[width * height];
+        }
+    }
+}
